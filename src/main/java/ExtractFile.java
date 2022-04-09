f589d4a46e50f558e037c0078c9a831103cb4658
Mes2611 <hypercpp1@gmail.com>
2022-04-10 01:44:05 +0200

    implémentation de la lecture du fichier

--- src/main/java/ExtractFile.java
+++ src/main/java/ExtractFile.java
@@ -1,9 +1,23 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class ExtractFile
-{
-    public List<String> readFile(File file) {
-        return null;
+public class ExtractFile {
+
+    public List<String> readFile(File file) throws FileNotFoundException {
+        List<String> extractLine = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                extractLine.add(scanner.nextLine());
+            }
+        }catch(FileNotFoundException fe) {
+            throw fe;
+        }
+
+        return extractLine;
     }
 }
